677102453587412e5e23dc8c5604c5a8df6bbbe9
dontolvidar <joradiazf@gmail.com>
2026-07-02 13:56:48 -0500

    6 creando un post manual de personaje

--- app/controllers/personaje_controller.rb
+++ app/controllers/personaje_controller.rb
@@ -2,4 +2,12 @@ class PersonajeController < ApplicationController
     def index
         render json: Personaje.all
     end
+    def create
+	    personaje=Personaje.new(nombre: 'Mickey', edad: 50, peso: 69.2, historia: 'a golden globe candidate', imagen: 'http://imagen.com/Mickey')
+        if personaje.save
+            render json:personaje, status: :created
+        else
+            render json:personaje.errors, status: :unproccessable_entity
+        end
+    end
 end
