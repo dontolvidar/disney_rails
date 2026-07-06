6e101267b402fe755a70317ae4d9761225811dc3
dontolvidar <joradiazf@gmail.com>
2026-07-06 11:25:55 -0500

    Proyecto Login autenticacion bien

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,13 +3,52 @@ class AuthController < ApplicationController
     rescue_from ActionController::ParameterMissing, with: :parameter_missing
     rescue_from AuthenticationError, with: :handle_authenticated
 
-    def create
+
+
+    def login
         user = User.find_by(username: params.require(:username))
-
-        raise AuthenticationError unless user && user.authenticate(params.require(:password))
+        raise AuthenticationError unless user&.authenticate(params.require(:password))
         token= AuthTokenService.call (user.id)
+        render json: {token: token}, status: :ok
+    rescue ActionController::ParameterMissing => e
+        render json: {errores:e.record.errors} , status: :unprocessable_entity
+    rescue ActiveRecord::RecordNotFound
+        render status: :unauthorized
+    rescue JWT::DecodeError
+        render status: :unauthorized
+    rescue AuthenticationError
+        render status: :unauthorized
+        
+    end
+    
+    
+    
+    def create
+        user = User.new(username: params.require(:username), password: params.require(:password))
+        if User.exists?(username: user.username)
+            render json: {errores:"El usuario ya existe"} , status: :unprocessable_entity
+            return
+        else
+            active_record = user.save
+            if active_record
+            
+            token= AuthTokenService.call (user.id)
+            render json: {creado:"El usuario se ha creado correctamente"} , status: :created
+            else
+                render json: user.errors, status: :unprocessable_entity
+            end
+        end
 
-        render json: {token: token}, status: :created
+        
+        
+        rescue ActionController::ParameterMissing => e
+            render json: {errores:e.record.errors} , status: :unprocessable_entity
+        rescue ActiveRecord::RecordNotFound
+            render status: :unauthorized
+        rescue JWT::DecodeError
+            render status: :unauthorized
+        rescue AuthenticationError
+            render status: :unauthorized
     end
 
     
